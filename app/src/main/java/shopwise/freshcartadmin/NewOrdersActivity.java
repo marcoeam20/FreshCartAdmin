72b07321ca5f072ef5901944bf34b39f5f24020d
macbook <eamaeguardo@gmail.com>
2017-10-08 17:55:52 +0800

    first commit

---
+++ app/src/main/java/shopwise/freshcartadmin/NewOrdersActivity.java
@@ -0,0 +1,13 @@
+package shopwise.freshcartadmin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewOrdersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_orders);
+    }
+}
